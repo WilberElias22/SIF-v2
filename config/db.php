14cb925e2abe8c1ec9fcc157cd94eaf7f82de347
WilberElias22 <ia.wilberelias@ufg.edu.sv>
2022-10-27 12:31:28 -0600

    first commit

---
+++ config/db.php
@@ -0,0 +1,24 @@
+<?php
+/*Datos de conexion a la base de datos*/
+define('DB_HOST', 'bnipdg3qz0lx73ey1yen-mysql.services.clever-cloud.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'ujddlpairhk7dots');//Usuario de tu base de datos
+define('DB_PASS', 'oM9mvUuocvN6fIxDLmnr');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'bnipdg3qz0lx73ey1yen');//Nombre de la base de datos
+ 
+/*Datos de la empresa*/
+define('NOMBRE_EMPRESA', 'SISTEMAS WEB LA');
+define('DIRECCION_EMPRESA', 'San Miguel, El Salvador, C.A.');
+define('TELEFONO_EMPRESA', '+(503) 2250-5550');
+define('EMAIL_EMPRESA', 'info@obedalvarado.pw');
+define('TAX', '13');
+
+/*-------------------------------------------------
+Los datos por defecto del usuario administrador son:
+#usuario: admin
+#contraseña: admin
+Los datos de inicio de sesion podran ser modificados 
+desde el panel de control del sistema
+---------------------------------------------------*/
+
+
+?>
