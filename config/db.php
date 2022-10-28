007dc227902b16267b4cc2f4e261dd57ad0d8e66
WilberElias22 <ia.wilberelias@ufg.edu.sv>
2022-10-27 18:59:49 -0600

    Cambio de DB

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 /*Datos de conexion a la base de datos*/
-define('DB_HOST', 'bnipdg3qz0lx73ey1yen-mysql.services.clever-cloud.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
-define('DB_USER', 'ujddlpairhk7dots');//Usuario de tu base de datos
-define('DB_PASS', 'oM9mvUuocvN6fIxDLmnr');//Contraseña del usuario de la base de datos
-define('DB_NAME', 'bnipdg3qz0lx73ey1yen');//Nombre de la base de datos
+define('DB_HOST', 'bj8fcdiss39lczsgsoiy-mysql.services.clever-cloud.com');//DB_HOST:  generalmente suele ser "127.0.0.1"
+define('DB_USER', 'uqbzsfmkgeitpzzr');//Usuario de tu base de datos
+define('DB_PASS', 'MaNBMVdLgBSyjqcd8Fxm');//Contraseña del usuario de la base de datos
+define('DB_NAME', 'bj8fcdiss39lczsgsoiy');//Nombre de la base de datos
  
 /*Datos de la empresa*/
 define('NOMBRE_EMPRESA', 'SISTEMAS WEB LA');
